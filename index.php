08de8878b1f69532f63dfa5dcc4e0fa079185376
Rafiqul Islam <rafique.ngn@gmail.com>
2013-06-27 04:58:51 +0600

    added a wp installation

--- index.php
+++ index.php
@@ -1,36 +1,17 @@
 <?php
 /**
- * Index
- *
- * Standard loop for the front-page
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
  * @package WordPress
- * @subpackage Foundation, for WordPress
- * @since Foundation, for WordPress 4.0
  */
 
-get_header(); ?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-    <!-- Main Content -->
-    <div class="large-9 columns" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
-
-		<?php else : ?>
-
-			<h2><?php _e('No posts.', 'foundation' ); ?></h2>
-			<p class="lead"><?php _e('Sorry about this, I couldn\'t seem to find what you were looking for.', 'foundation' ); ?></p>
-			
-		<?php endif; ?>
-
-		<?php foundation_pagination(); ?>
-
-    </div>
-    <!-- End Main Content -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+/** Loads the WordPress Environment and Template */
+require('./wp-blog-header.php');
