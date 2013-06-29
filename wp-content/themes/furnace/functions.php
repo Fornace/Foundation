f67953638f3e4c74ffe8665bc5b4e03b76938caa
Rafiqul Islam <rafique.ngn@gmail.com>
2013-06-30 00:00:49 +0600

    working on a theme

--- wp-content/themes/furnace/functions.php
+++ wp-content/themes/furnace/functions.php
@@ -4,7 +4,35 @@
  * @subpackage furnace
  * The Main functions.php file for "Furnace".
  */
-require_once dirname(__FILE__) . '/includes/init.php'
-function furnace_setup() {
+require_once dirname(__FILE__) . '/includes/init.php';
+if (!function_exists('furnace_setup')) :
 
+    function furnace_setup()
+    {
+        load_theme_textdomain('furnace', get_template_directory() . '/languages');
+
+        add_editor_style();
+
+        add_theme_support('post-thumbnails');
+        add_theme_support('automatic-feed-links');
+        add_theme_support('post-formats', array('aside', 'link', 'image', 'video', 'quote', 'status'));
+    }
+
+    add_action('after_theme_setup', 'furnace_setup');
+endif;
+
+
+if (!function_exists('furnace_manage_assets')) :
+function furnace_manage_assets()
+{
+    wp_deregister_script('jquery');
+
+    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.9/jquery.min.js');
+
+    wp_enqueue_script('fs_app', get_template_directory_uri() . '/assets/js/app.js', array('jquery'), '0.0.1', TRUE );
+    wp_enqueue_script('fs_app', get_template_directory_uri() . '/assets/js/script.js', array('jquery', 'app'), '0.0.1', TRUE );
+
+    wp_enqueue_style('fs_style', get_template_directory_uri() . '/assets/css/master.css');
 }
+add_action('wp_enqueue_scripts', 'furnace_manage_assets');
+endif;
