00136ac49b0e05a18d3c7d19569f5162a70e24d3
Rafiqul Islam <rafique.ngn@gmail.com>
2013-06-29 17:05:23 +0600

    little more

---
+++ wp-content/themes/furnace/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package Wordpress
+ * @subpackage furnace
+ * The Main functions.php file for "Furnace".
+ */
+require_once dirname(__FILE__) . '/includes/init.php'
+function furnace_setup() {
+
+}
