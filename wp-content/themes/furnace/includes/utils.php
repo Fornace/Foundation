7a607ef3754a9dec2f9033a59af7a6f7790fa9c6
Rafiqul Islam <rafique.ngn@gmail.com>
2013-06-30 18:28:03 +0600

    so far so good

--- wp-content/themes/furnace/includes/utils.php
+++ wp-content/themes/furnace/includes/utils.php
@@ -7,7 +7,6 @@
 function furnaceLoop()
 {
     global $query_string, $wp_query;
-    $defaults = $wp_query->query_vars;
 
     $paged = (get_query_var('paged')) ? get_query_var('paged') : (get_query_var('page')) ? get_query_var('page') : 1;
 
@@ -17,27 +16,14 @@ function furnaceLoop()
     $params = wp_parse_args($params, $query_string);
     if (func_num_args() > 0)
     {
-        /*if (is_array($args[0]))
-        {
-            $params = array()
-            $params = wp_parse_args($paged, $defaults);
-            $params = wp_parse_args($args[0], $defaults);
-        }
-        elseif (is_string($args[0]))
-        {
-            $params = wp_parse_args($paged, $query_string);
-            $params = wp_parse_args($args[0], $query_string);
-        }*/
-
         $params = wp_parse_args($args[0], $params);
 
-
+        var_dump($params);
         $loop = new WP_Query($params);
     }
     else {
         $loop = new WP_Query();
     }
-    var_dump($query_string);
-    var_dump($params);
+
     return $loop;
 }
--- wp-content/themes/furnace/index.php
+++ wp-content/themes/furnace/index.php
@@ -1,9 +1,9 @@
 <?php
 get_header();
 
-$l = furnaceLoop('posts_per_page=1');
-
+furnaceLoop('posts_per_page=-1');
+/*
 while($l->have_posts()) : $l->the_post();
-    the_title();
+    the_title('<h2>', '</h2>');
     the_content();
-endwhile;
+endwhile;*/
