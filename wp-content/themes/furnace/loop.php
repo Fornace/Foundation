75396cd36f1a7084cee27ac4992ff059d938c071
Rafiqul Islam <rafique.ngn@gmail.com>
2013-07-02 21:57:46 +0600

    404

--- wp-content/themes/furnace/loop.php
+++ wp-content/themes/furnace/loop.php
@@ -2,8 +2,6 @@
 /**
  * Default loop template
  */
-
-
 $loop = furnaceLoop(apply_filters('fs_loop_default_args', NULL));
 
 if ($loop->have_posts()) :
@@ -11,10 +9,15 @@ if ($loop->have_posts()) :
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <header>
                 <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+                <p><small><?php wp_meta(); ?></small></p>
             </header>
-
+            <section>
+                <?php the_content(); ?>
+            </section>
         </article>
 
     <?php endwhile;
+    else :
+    get_404_template();
 endif;
 ?>
