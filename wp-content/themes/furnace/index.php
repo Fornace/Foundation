966d2fb4a9583487d7c839f742da7a73654b7a36
Rafiqul Islam <rafique.ngn@gmail.com>
2013-07-04 14:44:23 +0600

    changed loop footprint

--- wp-content/themes/furnace/index.php
+++ wp-content/themes/furnace/index.php
@@ -1,7 +1,13 @@
 <?php
 
 get_header();
+$loop = furnaceLoop(apply_filters('fs_loop_default_args', NULL));
 
+if ($loop->have_posts()) :
+    while ($loop->have_posts()) : $loop->the_post();
 get_template_part('loop', get_post_format());
-
+endwhile;
+else :
+    get_404_template();
+endif;
 get_footer();
--- wp-content/themes/furnace/loop.php
+++ wp-content/themes/furnace/loop.php
@@ -2,10 +2,7 @@
 /**
  * Default loop template
  */
-$loop = furnaceLoop(apply_filters('fs_loop_default_args', NULL));
 
-if ($loop->have_posts()) :
-    while ($loop->have_posts()) : $loop->the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <header>
                 <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
@@ -15,9 +12,3 @@ if ($loop->have_posts()) :
                 <?php the_content(); ?>
             </section>
         </article>
-
-    <?php endwhile;
-    else :
-    get_404_template();
-endif;
-?>
