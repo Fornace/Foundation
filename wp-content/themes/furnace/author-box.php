0ee989d9ff692b64c7f8631514eb04c9bd829349
Rafiqul Islam <rafique.ngn@gmail.com>
2013-07-08 09:28:18 +0600

    fkdfk

--- wp-content/themes/furnace/author-box.php
+++ wp-content/themes/furnace/author-box.php
@@ -6,7 +6,7 @@
 
         <div>
             <a href="<?php get_the_author_meta('url'); ?>"><?php echo get_avatar( get_the_author_meta('user_email'),'60' ); ?></a>
-            <h5><?php _e('About', 'furnace' ); ?> <?php the_author_link(); ?></h5>
+            <h5><?php _e('About', 'furnace' ); ?> <?php the_author_posts_link(); ?></h5>
             <p>
                 <?php echo get_the_author_meta('description'); ?>
             </p>
