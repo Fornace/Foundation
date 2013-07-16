aa7f138fcafd920059b17d90590c3e965d885f18
Rafiqul Islam <rafique.ngn@gmail.com>
2013-07-16 20:18:07 +0600

    layout for quick prototype

---
+++ HTML/assets/js/app/main.js
@@ -0,0 +1,5 @@
+(function($){
+    $.ajax({
+        url: location.href;
+    });
+})(jQuery);
---
+++ HTML/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>My Home Page</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+        <link rel="stylesheet" type="text/css" href="assets/css/master.css" media="screen">
+
+    </head>
+    <body>
+        <!-- HTML body goes here... -->
+        <script type="text/javascript" src="assets/js/lib/jquery.js"></script>
+    </body>
+</html>
